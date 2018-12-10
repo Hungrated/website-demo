0c23048458107da67d3d9f316ccdf777c8f34405
hungrated <zhang295415658@qq.com>
2018-12-10 19:43:33 +0800

    master: modify header & footer

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
 
+  const headerElement = $('.J_header');
+
+  function changeHeader () {
+    headerElement.css('background', '#1855d1');
+  }
+
+  function restoreHeader () {
+    headerElement.css('background', 'transparent');
+  }
+
+  $(document).scroll(function () {
+    if($(document).scrollTop() !== 0) {
+      changeHeader();
+    } else {
+      restoreHeader();
+    }
+  })
+
 });
